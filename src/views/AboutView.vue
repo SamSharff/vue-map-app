0b8919c2fc7876c30408750163ad0ab1d76438c5
Sam Sharff <samanthaleec@gmail.com>
2022-04-20 16:01:02 -0400

    adds map and marker

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,6 +2,14 @@
 /* global mapboxgl*/
 
 export default {
+  // data: function () {
+  //   return {
+  //     places: [
+  //       { lat: -25.363, lng: 131.044, description: "A place in Australia" },
+  //       { lat: -33.8675, lng: 151.207, description: "The main city!" },
+  //     ],
+  //   };
+  // },
   mounted: function () {
     mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_API_KEY;
     const map = new mapboxgl.Map({
@@ -10,7 +18,11 @@ export default {
       center: [-69.0648, 44.2098], // starting position [lng, lat]
       zoom: 9, // starting zoom
     });
-    console.log(map);
+    // Create a default Marker and add it to the map.
+    const marker1 = new mapboxgl.Marker().setLngLat([-69.0648, 44.2098]).addTo(map);
+    // create the popup
+    const popup = new mapboxgl.Popup({ offset: 25 }).setText("Welcome to Camden, Maine!");
+    console.log(map, marker1, popup);
   },
 };
 </script>
@@ -32,5 +44,17 @@ body {
   /* bottom: 0; */
   height: 300px;
   width: 100%;
+}
+#marker {
+  background-image: url("https://media.istockphoto.com/photos/view-from-mount-battie-overlooking-camden-harbor-maine-picture-id1176662480?k=20&m=1176662480&s=612x612&w=0&h=mJRo1s7-WO3LSFLc6oa0qRkiavW3TAAPi2k-afbpvyU=");
+  background-size: cover;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  cursor: pointer;
+}
+
+.mapboxgl-popup {
+  max-width: 200px;
 }
 </style>
